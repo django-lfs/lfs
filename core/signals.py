5c7a0dc9386296afe1a2fb8a5eab8b13b8ce777d
kai.diefenbach@iqpp.de <kai.diefenbach@iqpp.de@70813176-cce2-11dd-87b6-9d1a99559f42>
2009-04-01 21:50:26 +0000

    Added TODO Tag.
    git-svn-id: http://django-lfs.googlecode.com/svn/trunk/lfs@92 70813176-cce2-11dd-87b6-9d1a99559f42

--- core/signals.py
+++ core/signals.py
@@ -15,4 +15,7 @@ order_submitted = django.dispatch.Signal()
 
 # Property
 property_type_changed = django.dispatch.Signal()
+
+# TODO: Replace this with "m2m_changed" when available, or think about to use
+# an explicit relation ship class
 product_removed_property_group = django.dispatch.Signal()

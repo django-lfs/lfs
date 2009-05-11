e603edd5df574df565cd93adb5566fb83505890d
kai.diefenbach@iqpp.de <kai.diefenbach@iqpp.de@70813176-cce2-11dd-87b6-9d1a99559f42>
2009-05-11 05:53:35 +0000

    Update tinymce
    git-svn-id: http://django-lfs.googlecode.com/svn/lfs/trunk@285 70813176-cce2-11dd-87b6-9d1a99559f42

--- static/js/lfs.manage.js
+++ static/js/lfs.manage.js
@@ -1,20 +1,20 @@
 tinyMCE.init({
     mode: "none",
     theme : "advanced",
+    height : "400",
     tab_focus : ":prev,:next",
     button_tile_map : true,
     plugins : "safari, fullscreen",
     theme_advanced_buttons1 : "bold, italic, underline, |, justifyleft," +  
                               "justifycenter, justifyright, justifyfull, |," + 
-                              "bullist,numlist, |, outdent, indent, |,undo," +  
-                              "redo, |, code, styleselect, formatselect, |," +  
+                              "bullist,numlist, |, outdent, indent, |, image, |, undo," +  
+                              "redo, |, code, link, unlink, styleselect, formatselect, |," +  
                               "removeformat, fullscreen",
     theme_advanced_buttons2: "",
     theme_advanced_buttons3: "",
     theme_advanced_buttons4: "",
     theme_advanced_toolbar_location : "top",
-    theme_advanced_toolbar_align : "left",
-    
+    theme_advanced_toolbar_align : "left",    
     content_css : "../css/tiny_mce_styleselect.css"
     
 })

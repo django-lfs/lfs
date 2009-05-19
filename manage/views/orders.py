11081e74c7ebff658568b56b19ed5ba7cf9b032c
kai.diefenbach@iqpp.de <kai.diefenbach@iqpp.de@70813176-cce2-11dd-87b6-9d1a99559f42>
2009-05-19 04:05:11 +0000

    Issue 11: Added import of ugettext_lazy
    git-svn-id: http://django-lfs.googlecode.com/svn/lfs/trunk@312 70813176-cce2-11dd-87b6-9d1a99559f42

--- manage/views/orders.py
+++ manage/views/orders.py
@@ -4,6 +4,7 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.utils.translation import ugettext_lazy as _
 
 # lfs imports
 import lfs.core.utils

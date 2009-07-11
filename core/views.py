d1ff427d973127d04ead0eb38d701703aa83d680
kai.diefenbach@iqpp.de <kai.diefenbach@iqpp.de@70813176-cce2-11dd-87b6-9d1a99559f42>
2009-07-11 18:24:16 +0000

    505handler: send also traceback if an error occurs
    git-svn-id: http://django-lfs.googlecode.com/svn/lfs/trunk@668 70813176-cce2-11dd-87b6-9d1a99559f42

--- core/views.py
+++ core/views.py
@@ -1,3 +1,6 @@
+# python imports
+import sys, traceback
+
 # django imports
 from django.conf import settings
 from django.core.mail import EmailMessage
@@ -26,6 +29,15 @@ def robots(request, template_name="shop/robots.txt"):
 def server_error(request):
     """Own view in order to pass RequestContext and send an error message.
     """
+    exc_type, exc_info, tb = sys.exc_info()
+    response = "%s\n" % exc_type.__name__
+    response += "%s\n" % exc_info
+    response += "TRACEBACK:\n"
+    for tb in traceback.format_tb(tb):
+        response += "%s\n" % tb
+
+    response += "\nREQUEST:\n%s" % request
+
     try:
         from_email = settings.ADMINS[0][1]
         to_emails = [a[1] for a in settings.ADMINS]
@@ -33,8 +45,8 @@ def server_error(request):
         pass
     else:
         mail = EmailMessage(
-            subject="Error LFS", body=request, from_email=from_email, to=to_emails)
+            subject="Error LFS", body=response, from_email=from_email, to=to_emails)
         mail.send(fail_silently=True)
 
     t = loader.get_template('500.html')
-    return HttpResponse(t.render(request), status=500)
+    return HttpResponse(t.render(RequestContext(request)), status=500)

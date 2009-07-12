601142d70d8dcfcbe1472d65afe103333a3736e0
kai.diefenbach@iqpp.de <kai.diefenbach@iqpp.de@70813176-cce2-11dd-87b6-9d1a99559f42>
2009-07-12 09:11:35 +0000

    More improvements for cart management.
    git-svn-id: http://django-lfs.googlecode.com/svn/lfs/trunk@677 70813176-cce2-11dd-87b6-9d1a99559f42

--- manage/views/carts.py
+++ manage/views/carts.py
@@ -1,5 +1,6 @@
 # django imports
 from django.contrib.auth.decorators import permission_required
+from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
@@ -12,9 +13,22 @@ from lfs.cart.models import Cart
 def carts_view(request, template_name="manage/cart/carts.html"):
     """Displays all orders.
     """
-    carts = []
-    for cart in Cart.objects.all().order_by("-creation_date"):
+
+    try:
+        start = int(request.GET.get("start", 0))
+    except ValueError:
+        start = 0
+
+    try:
+        amount = int(request.GET.get("amount", 10))
+    except ValueError:
+        amount = 0
+
+    end = start + amount
         
+    carts = []
+    for cart in Cart.objects.all().order_by("-creation_date")[start:end]:
+
         products = []
         total = 0
         for item in cart.items():
@@ -22,16 +36,46 @@ def carts_view(request, template_name="manage/cart/carts.html"):
             if item.product.name != "":
                 products.append(item.product.name)
 
+        if cart.user:
+            if cart.user.first_name:
+                user_name = cart.user.first_name + " "
+            if cart.user.last_name:
+                user_name += cart.user.last_name
+        else:
+            user_name = None
+
         carts.append({
             "id" : cart.id,
             "amount_of_items" : cart.amount_of_items,
+            "session" : cart.session,
+            "user" : cart.user,
             "total" : total,
             "products" : ", ".join(products),
             "creation_date" : cart.creation_date,
+            "user_name" : user_name,
         })
+    
+    if start == 0:
+        previous_url = None
+    elif (start - amount) < 0:
+        previous_url = reverse("lfs_manage_carts") + "?start=0"
+    else:
+        previous_url = reverse("lfs_manage_carts") + "?start=%s" % (start - amount)
 
+    if (start > len(Cart.objects.all())):
+        next_url = None
+    else:
+        next_url = reverse("lfs_manage_carts") + "?start=%s" % end
+    
+    if previous_url and amount:
+        previous_url += "&amount=%s" % amount
+    if next_url and amount:
+        next_url += "&amount=%s" % amount
+        
     return render_to_response(template_name, RequestContext(request, {
         "carts" : carts,
+        "previous_url" : previous_url,
+        "next_url" : next_url,
     }))
 
 @permission_required("manage_shop", login_url="/login/")
@@ -40,13 +84,23 @@ def cart_view(request, cart_id, template_name="manage/cart/cart.html"):
     """
     cart = lfs_get_object_or_404(Cart, pk=cart_id)
     carts = Cart.objects.all().order_by("-creation_date")
-    
+
     total = 0
     for item in cart.items():
         total += item.get_price_gross()
 
+    if cart.user:
+        if cart.user.first_name:
+            user_name = cart.user.first_name + " "
+        if cart.user.last_name:
+            user_name += cart.user.last_name
+    else:
+        user_name = None
+
     return render_to_response(template_name, RequestContext(request, {
-        "current_cart" : cart,
+        "cart" : cart,
+        "user_name" : user_name,
         "carts" : carts,
         "total" : total,
+
     }))

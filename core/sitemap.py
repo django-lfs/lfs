91b0dcaa5b3e5701df702610ce6c2efb382d1653
kai.diefenbach@gmail.com <kai.diefenbach@gmail.com@70813176-cce2-11dd-87b6-9d1a99559f42>
2009-07-27 10:04:43 +0000

    Added sitemap for categories.
    git-svn-id: http://django-lfs.googlecode.com/svn/lfs/trunk@706 70813176-cce2-11dd-87b6-9d1a99559f42

--- core/sitemap.py
+++ core/sitemap.py
@@ -1,4 +1,9 @@
+# python imports
+from datetime import datetime
+
+# improt django
 from django.contrib.sitemaps import Sitemap
+from lfs.catalog.models import Category
 from lfs.catalog.models import Product
 
 class ProductSitemap(Sitemap):
@@ -8,7 +13,19 @@ class ProductSitemap(Sitemap):
     priority = 0.5
 
     def items(self):
-        return Product.objects.all()
+        return Product.objects.filter(active=True)
 
     def lastmod(self, obj):
         return obj.creation_date
+        
+class CategorySitemap(Sitemap):
+    """Google's XML sitemap for products.
+    """
+    changefreq = "weekly"
+    priority = 0.5
+
+    def items(self):
+        return Category.objects.all()
+
+    def lastmod(self, obj):
+        return datetime.now()

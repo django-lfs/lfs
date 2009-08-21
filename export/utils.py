be58abbd0b6fabb17bf178c36c6afe0e498f255e
kai.diefenbach@gmail.com <kai.diefenbach@gmail.com@70813176-cce2-11dd-87b6-9d1a99559f42>
2009-08-21 16:12:10 +0000

    lfs.export: check whether a script already exists (otherwise sqlite seems to get locked)
    git-svn-id: http://django-lfs.googlecode.com/svn/lfs/trunk@801 70813176-cce2-11dd-87b6-9d1a99559f42

--- export/utils.py
+++ export/utils.py
@@ -5,8 +5,12 @@ def register(method, name):
     """Registers a new export logic.
     """
     try:
-        Script.objects.create(
-            module=method.__module__, method=method.__name__, name=name)
-    except:
-        # Fail silently
-        pass
+        Script.objects.get(
+            module=method.__module__, method=method.__name__)
+    except Script.DoesNotExist:
+        try:
+            Script.objects.create(
+                module=method.__module__, method=method.__name__, name=name)
+        except:
+            # Fail silently
+            pass

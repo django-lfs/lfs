5d3456e8541b833f5f3fb69f01f3731ff77ea239
kai.diefenbach@gmail.com <kai.diefenbach@gmail.com@70813176-cce2-11dd-87b6-9d1a99559f42>
2009-08-28 06:20:41 +0000

    lfs.cart: added missing import of Http404
    git-svn-id: http://django-lfs.googlecode.com/svn/lfs/trunk@841 70813176-cce2-11dd-87b6-9d1a99559f42

--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,7 @@
-
-# # django imports
+# django imports
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.urlresolvers import reverse
+from django.http import Http404
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
